86f2eae5fdaae4b06e83eb19506388fdc487a3bf
Naruto <natsuna@163.com>
2015-06-25 19:54:18 +0800

    150624：圆点随图片变动，next的逻辑
    主要考虑第五张图的next情况

--- 150624.js
+++ 150624.js
@@ -8,18 +8,37 @@ window.onload = function(){
 	var left = parseInt(pic.style.left);
 
 	next.onclick = function(){
+		showBut();
 		if (parseInt(pic.style.left) <= -3000) { /*无限滚动*/
-			pic.style.left = -600 + 'px';
+			pic.style.left = -600 + 'px';	
 			return;
 		};
-		
 		pic.style.left = parseInt(pic.style.left) -600 + 'px';
 	}
 	prev.onclick = function(){
+		showBut();
 		if (parseInt(pic.style.left) >= -600) { /*无限滚动*/
 			pic.style.left = -3000 + 'px';
 			return;
 		};
 		pic.style.left = parseInt(pic.style.left) +600 + 'px';
+	}
+
+	/*function showBut(){
+		for(var i = 0, length1 = but.length; i < length1; i++){
+			but[i].className = 'on';
+		}
+	}*/
+
+	function showBut(){
+		var demo = parseInt(pic.style.left)/(-600);
+		/*点击next时的逻辑*/
+		if (demo == 5) {
+			but[demo-1].className = '';
+			but[0].className = 'on';
+			return;
+		};
+		but[demo].className = 'on';
+		but[demo-1].className = '';
 	}
 }
