4339f9dc1b981d4481aafbb368ec4f5a576521c0
Naruto <natsuna@163.com>
2015-06-27 13:50:16 +0800

    150626：定时器自动播放

--- 150626.js
+++ 150626.js
@@ -1,3 +1,16 @@
 window.onload = function(){
-	
+	var pic = document.getElementById('pic');
+	var interval = 3000;
+
+	function play(){
+		if(parseInt(pic.style.left) <= -3000){
+			pic.style.left = -600 + 'px';
+		}
+		var timer = setTimeout(function() {
+			pic.style.left = -600 + parseInt(pic.style.left) + 'px';
+			play();
+		}, interval);
+	}
+
+	play();
 }
