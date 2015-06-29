4a7a29f64d00c6c9827c7433e04925b911e6e87b
Naruto <natsuna@163.com>
2015-06-29 09:22:40 +0800

    150626：悬浮时清除定时器

--- 150626.js
+++ 150626.js
@@ -1,14 +1,16 @@
 window.onload = function(){
+	var content = document.getElementById('content');
 	var pic = document.getElementById('pic');
 	var but = document.getElementById('but').getElementsByTagName('span');
 	var index = 5;
 	var interval = 1000;
+	var timer;
 
 	function play(){
 		if(parseInt(pic.style.left) < -3000){
 			pic.style.left = -600 + 'px';
 		}
-		var timer = setTimeout(function() {
+		timer = setTimeout(function() {
 			pic.style.left = -600 + parseInt(pic.style.left) + 'px';
 			if(index == 1){
 				index = 6;
@@ -18,8 +20,13 @@ window.onload = function(){
 			play();
 		}, interval);
 	}
+	function stop(){
+		clearTimeout(timer);
+	}
 
 	play();
+	content.onmouseover = stop;
+	content.onmouseout = play;
 
 	function buton(index){
 		for (var i = 0; i < but.length; i++) {
