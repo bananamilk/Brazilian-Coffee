f8af7bba9fde21de63ec5fd80440a23560128f67
Naruto <natsuna@163.com>
2015-07-30 15:37:25 +0800

    150626：dt的切换，未完待续

--- 150626.js
+++ 150626.js
@@ -2,16 +2,22 @@ window.onload = function(){
 	var content = document.getElementById('content');
 	var pic = document.getElementById('pic');
 	var but = document.getElementById('but').getElementsByTagName('span');
+	var con = document.getElementById('con');
+	var dt = con.getElementsByTagName('dt');
+	var dl = con.getElementsByTagName('dl');
 	var index = 5;
 	var interval = 1000;
 	var timer;
+	var coni = 0;
 
 	function play(){
 		if(parseInt(pic.style.left) < -3000){
 			pic.style.left = -600 + 'px';
+			coni = 0;
 		}
 		timer = setTimeout(function() {
 			pic.style.left = -600 + parseInt(pic.style.left) + 'px';
+			dt.style.top = -38 + parseInt(dt.style.top) + 'px';
 			if(index == 1){
 				index = 6;
 			}
